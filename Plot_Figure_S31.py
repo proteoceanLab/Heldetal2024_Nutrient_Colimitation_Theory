284eee9805d5b9f55dab3b9b9eb21e0f197dcddb
Noelle Held <noelleheld@gmail.com>
2024-10-20 12:39:22 -0700

    correct plotting code for S31
    need to skip more rows after update to Dataset S3

--- Plot_Figure_S31.py
+++ Plot_Figure_S31.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import colimitation_plots
+import numpy as np
 
 plt.rcParams = colimitation_plots.SetrcParams(plt.rcParams)
 '''plt.rcParams.update({
@@ -9,8 +10,8 @@ plt.rcParams = colimitation_plots.SetrcParams(plt.rcParams)
     "font.size": 20})'''
 plt.rcParams['lines.linewidth'] = 2
 
-df = pd.read_excel("Data/Dataset_S3.xlsx", skiprows=4)
-df = df.iloc[:-3,]
+df = pd.read_excel("Data/Dataset_S3.xlsx", skiprows=11)
+#df = df.iloc[:-3,]
 df2 = pd.DataFrame()
 df2['Liebig_Monod'] = pd.concat([df['Nutrient 1 Li_liebig'], df['Nutrient 2 Li_liebig']])
 df2['Multiplicative_Monod'] = pd.concat([df['Nutrient 1 Li_multiplicative'], df['Nutrient 2 Li_multiplicative']])
@@ -19,10 +20,11 @@ df2['PAT'] = pd.concat([df['Nutrient 1 Li_pat'], df['Nutrient 2 Li_pat']])
 
 
 fig, ax = plt.subplots(4,2, figsize=(6,6))
-ax[0,0].hist(df['Meff_liebig'], bins=5, color='#0504aa', align='mid', rwidth=0.85)
-ax[1,0].hist(df['Meff_PAT'], bins=5, color='#0504aa', align='left',rwidth=0.95)
-ax[2,0].hist(df['Meff_additive'], bins=5, color='#0504aa', align='left',rwidth=0.9)
-ax[3,0].hist(df['Meff_multiplicative'], bins=5, color='#0504aa', align='left', rwidth=0.85)
+bins = np.histogram(np.hstack((df['Meff_liebig'], df['Meff_PAT'])), bins=5)[1]
+ax[0,0].hist(df['Meff_liebig'], bins, color='#0504aa', align='left', rwidth=0.95)
+ax[1,0].hist(df['Meff_PAT'], bins, color='#0504aa', align='left',rwidth=0.95)
+ax[2,0].hist(df['Meff_additive'], bins, color='#0504aa', align='left',rwidth=0.95)
+ax[3,0].hist(df['Meff_multiplicative'], bins, color='#0504aa', align='left', rwidth=0.95)
 ax[3,0].set_xlabel('Number of rate limiting resources $M^\mathrm{rate}_\mathrm{eff}$')
 
 for axis in [ax[0,0],ax[1,0],ax[2,0],ax[3,0]]:
@@ -30,11 +32,13 @@ for axis in [ax[0,0],ax[1,0],ax[2,0],ax[3,0]]:
     axis.set_ylim([0,22])
     axis.set_xticks([1,1.25,1.5,1.75,2])
     axis.set_yticks([0,5,10,15,20])
+print(bins)
 
-ax[0,1].hist(df2['Liebig_Monod'], bins=5, color='#0504aa', align='left')
-ax[1,1].hist(df2['PAT'], bins=5, color='#0504aa', align='left',rwidth=0.8)
-ax[2,1].hist(df2['Additive'], bins=5, color='#0504aa', align='left',rwidth=0.8)
-ax[3,1].hist(df2['Multiplicative_Monod'], bins=5, color='#0504aa', align='left',rwidth=0.8)
+bins = np.histogram(np.hstack((df2['Liebig_Monod'], df2['PAT'])), bins=5)[1]
+ax[0,1].hist(df2['Liebig_Monod'], bins=bins, color='#0504aa', align='left', rwidth=0.95)
+ax[1,1].hist(df2['PAT'], bins=bins, color='#0504aa', align='left',rwidth=0.95)
+ax[2,1].hist(df2['Additive'], bins=bins, color='#0504aa', align='left',rwidth=0.95)
+ax[3,1].hist(df2['Multiplicative_Monod'], bins=bins, color='#0504aa', align='left',rwidth=0.95)
 ax[3,1].set_xlabel('Rate limitation coefficient $L^\mathrm{rate}i$')
 
 rows = ['Liebig\nMonod', 'PAT', 'Additive', 'Mult.\nMonod']
